9100610f0e20181fe532b2f6ceae8c6dc3209a57
Alethepata <alessia.patane@gmail.com>
2024-06-12 15:27:28 +0200

    edit card

--- src/components/partials/Card.jsx
+++ src/components/partials/Card.jsx
@@ -6,16 +6,18 @@ function Card() {
             <figure>
                 <img src={img} alt=""/>
             </figure>
-            <div className="card-text">
-                <h3>
-                    Titolo del Post
-                </h3>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil necessitatibus beatae, veniam fuga similique illo iure itaque dolorem rem sed, aspernatur voluptatibus voluptates? Vel cum dolorum, aliquam tempora possimus modi quos unde. Nulla unde ipsa voluptatem facere accusamus delectus aliquid enim rem rerum in recusandae consequatur, dolore laborum distinctio consectetur eius iusto placeat. Omnis fugit, pariatur atque laboriosam doloremque itaque.
-                </p>
-            </div>
-            <div className="card-action">
-                <button>Leggi di più</button>
+            <div className="card-content">
+                <div className="card-text">
+                    <h3>
+                        Titolo del Post
+                    </h3>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil necessitatibus beatae, veniam fuga similique illo iure itaque dolorem rem sed, aspernatur voluptatibus voluptates? Vel cum dolorum, aliquam tempora possimus modi quos unde. Nulla unde ipsa voluptatem facere accusamus delectus aliquid enim rem rerum in recusandae consequatur, dolore laborum distinctio consectetur eius iusto placeat. Omnis fugit, pariatur atque laboriosam doloremque itaque.
+                    </p>
+                </div>
+                <div className="card-action">
+                    <button className="button">Leggi di più</button>
+                </div>
             </div>
 
         </div>
